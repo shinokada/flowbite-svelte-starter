57e0d02648264db3971fbb1d15cbd4fe469db3ef
Shinichi Okada <okada.shin@gmail.com>
2022-05-15 18:24:28 +0900

    feat: add Svelte-Sidebar-Menu demo

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,147 @@
 <script>
 	import '../app.css';
-	import { DarkMode } from 'flowbite-svelte';
-	let darkmodebtn =
-		'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-lg p-2.5 fixed right-4 top-2 z-50';
+	import { SidebarItem, DarkMode } from 'flowbite-svelte';
+	import { ChartPieSolid, ShoppingBagSolid, TableSolid, KeySolid } from 'svelte-heros';
+
+	import { Side, Nav, Responsive } from 'svelte-sidebar-menu';
+
+	import { quartInOut } from 'svelte/easing';
+
+	// Darkmode component
+	let btnClass =
+		'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 fixed right-2 top-24 z-50';
+
+	// Side component
+	let siteName = 'Svelte-Sidebar';
+
+	let siteClass = 'w-1/4 h-12 text-lg pt-3 pl-12';
+
+	let headerClass =
+		'bg-white px-10 items-center text-gray-600 border-b-2 p-4 dark:bg-gray-900 dark:text-white';
+
+	let hamburgerClass =
+		'hover:text-gray-500 cursor-pointer mr-4 border-none focus:outline-none pt-2 lg:hidden';
+
+	let topMenuDiv = 'container flex flex-wrap justify-end mx-auto pt-0 md:pt-2';
+
+	let topul =
+		'flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium pt-1 bg-white dark:bg-gray-900 dark:text-white';
+
+	let childLi =
+		'block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-lg dark:text-white';
+
+	let asideClass =
+		'absolute w-auto border-r-2 shadow-lg z-50 bg-white h-screen overflow-scroll dark:bg-gray-900 dark:text-white';
+
+	let spanClass =
+		'pl-2 self-center text-lg font-semibold text-gray-900 whitespace-nowrap dark:text-white';
+
+	// Nav component
+	let navClass = 'py-8 px-4 text-lg';
+
+	let navDivClass = 'pb-8';
+
+	let transitionParams = {
+		duration: 500,
+		delay: 100,
+		easing: quartInOut,
+		x: -200
+	};
+
+	const topMenuList = [
+		{ href: '/test1', name: 'Test 1' },
+		{ href: '/test2', name: 'Test 2' },
+		{ href: '/about', name: 'About' }
+	];
+
+	let uid = 1;
+	let sidebarItem = [
+		{
+			id: uid++,
+			name: 'Svelte-Sidebar',
+			href: '/',
+			icon: ChartPieSolid,
+			iconSize: 16,
+			iconClass: 'text-red-500'
+		},
+		{
+			id: uid++,
+			name: 'Subdir 1',
+			icon: ShoppingBagSolid,
+			iconSize: 16,
+			iconClass: 'text-blue-500',
+			children: [
+				{
+					id: uid++,
+					name: 'Test 1-1',
+					href: '/test1/'
+				},
+				{
+					id: uid++,
+					name: 'Test 1-2',
+					href: '/test1/test-2'
+				},
+				{
+					id: uid++,
+					name: 'Test 1-3',
+					href: '/test1/test-3'
+				}
+			]
+		},
+		{
+			id: uid++,
+			name: 'Subdir 2',
+			icon: KeySolid,
+			iconSize: 16,
+			iconClass: 'text-green-500',
+			children: [
+				{
+					id: uid++,
+					name: 'Test 2-1',
+					href: '/test2/'
+				},
+				{
+					id: uid++,
+					name: 'Test 2-2',
+					href: '/test2/test-2'
+				},
+				{
+					id: uid++,
+					name: 'Test 2-3',
+					href: '/test2/test-3'
+				}
+			]
+		},
+		{
+			id: uid++,
+			name: 'About',
+			icon: TableSolid,
+			iconSize: 16,
+			iconClass: 'text-purple-500',
+			href: '/about'
+		}
+	];
 </script>
 
-<DarkMode btnClass={darkmodebtn} />
-<slot />
+<DarkMode {btnClass} />
+<Responsive />
+<Side
+	{siteName}
+	{siteClass}
+	{headerClass}
+	{hamburgerClass}
+	{topMenuDiv}
+	{topul}
+	{childLi}
+	{asideClass}
+	{spanClass}
+	{transitionParams}
+	topMenus={topMenuList}
+>
+	<Nav {navClass} {navDivClass}>
+		<SidebarItem links={sidebarItem} />
+	</Nav>
+</Side>
+<main class="container mx-auto py-32 px-8 lg:pl-80 dark:text-white ">
+	<slot />
+</main>
---
+++ src/routes/test1/index.svelte
@@ -0,0 +1,28 @@
+<h1 class="text-4xl dark:text-white">Test 1-1</h1>
+<p class="dark:text-white">
+	Attack feet behind the couch destroy couch flop over give attitude hide when guests come over
+	hopped up on goofballs hunt anything that moves rub face on everything need to chase tail lick
+	butt chase imaginary bugs, all of a sudden go crazy leave dead animals as gifts bag stretch sweet
+	beast under the bed intently sniff hand why must they do that intently stare at the same spot swat
+	at dog claw drapes rub face on everything. Flop over behind the couch chase imaginary bugs give
+	attitude swat at dog sweet beast under the bed all of a sudden go crazy leave dead animals as
+	gifts claw drapes, why must they do that rub face on everything hopped up on goofballs bag stretch
+	attack feet hide when guests come over intently sniff hand lick butt destroy couch, need to chase
+	tail intently stare at the same spot hunt anything that moves give attitude all of a sudden go
+	crazy sweet beast under the bed rub face on everything . Leave dead animals as gifts destroy couch
+	swat at dog sweet beast under the bed rub face on everything bag stretch hide when guests come
+	over attack feet chase imaginary bugs all of a sudden go crazy claw drapes, intently stare at the
+	same spot lick butt need to chase tail hopped up on goofballs behind the couch flop over intently
+	sniff hand give attitude why must they do that, hunt anything that moves all of a sudden go crazy
+	behind the couch why must they do that flop over intently stare at the same spot bag stretch
+	hopped up on goofballs. Attack feet behind the couch all of a sudden go crazy swat at dog why must
+	they do that hunt anything that moves give attitude leave dead animals as gifts chase imaginary
+	bugs, lick butt hopped up on goofballs need to chase tail claw drapes hide when guests come over
+	intently sniff hand bag stretch rub face on everything, destroy couch sweet beast under the bed
+	intently stare at the same spot flop over rub face on everything attack feet why must they do that
+	behind the couch. Bag stretch claw drapes all of a sudden go crazy behind the couch destroy couch
+	hopped up on goofballs chase imaginary bugs sweet beast under the bed intently sniff hand attack
+	feet lick butt , flop over need to chase tail hide when guests come over give attitude swat at dog
+	rub face on everything intently stare at the same spot hunt anything that moves leave dead animals
+	as gifts why must they do that.
+</p>
