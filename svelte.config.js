a64101524de8002c8319f8a7f530a988c3b749cc
Shinichi Okada <okada.shin@gmail.com>
2022-05-24 15:08:28 +0900

    feat: add adapter-static and update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = "production" === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +14,17 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: "docs",
+			assets: "docs"
+		}),
+		
+		paths: {
+			// change below to your repo name
+			base: dev ? "" : "/flowbite-svelte-starter"
+		},
+	
+		target: "#svelte"
 	}
 };
 
