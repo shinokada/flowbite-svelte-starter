8778cf7b57c19bb91831615aa1e3f79c581b16ad
Shinichi Okada <okada.shin@gmail.com>
2022-07-01 11:49:13 +0200

    fix: removed footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,29 +1,9 @@
 <script>
 	import '../app.css';
-	import {
-		DarkMode,
-		Navbar,
-		NavBrand,
-		NavHamburger,
-		NavUl,
-		NavLi,
-		LogoFooter
-	} from 'flowbite-svelte';
+	import { DarkMode, Navbar, NavBrand, NavHamburger, NavUl, NavLi } from 'flowbite-svelte';
 	let darkmodebtn =
 		'text-gray-500 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-lg p-2.5 fixed right-4 top-32 z-50';
 	let navClass = 'bg-ricecake border-gray-200 p-8 border-b rounded  dark:bg-blackpearl';
-	// footer
-	let site = {
-		href: '/',
-		name: 'Flowbite Svelte',
-		img: 'https://via.placeholder.com/35.png/09f/fff'
-	};
-	let links = [
-		{ name: 'About', href: '/pages/about' },
-		{ name: 'Profile', href: '/' },
-		{ name: 'Contact', href: '/' }
-	];
-	let footerClass = 'p-4 bg-ricecake rounded-lg shadow md:px-6 md:py-8 dark:bg-blackpearl';
 </script>
 
 <DarkMode btnClass={darkmodebtn} />
@@ -49,4 +29,3 @@
 <main class="container mx-auto p-16 h-screen">
 	<slot />
 </main>
-<LogoFooter {site} {links} {footerClass} />
