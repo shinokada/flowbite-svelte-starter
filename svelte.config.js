83500e1188623621712cf697229b2b4d85a0fd65
Shinichi Okada <okada.shin@gmail.com>
2023-02-13 05:59:47 +0900

    fix: files update to sveltekit 1.5.6

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,9 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter()
