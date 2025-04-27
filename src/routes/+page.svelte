b09c6ec55c0421ed487776bb9f96ffa7866b8ba2
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-04-27 09:47:51 +0200

    fix: #6

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
   import { Alert } from 'flowbite-svelte';
 </script>
 
-<div class="relative mx-auto mt-8 h-full max-w-7xl overflow-y-auto px-8 pb-20">
-	<h1 class="my-8 flex justify-center">Flowbite Svelte Starter</h1>
+<div class="p-8">
+	<h1 class="my-8">Flowbite Svelte Starter</h1>
   <Alert>
     <span class="font-medium">Alert example</span>
     Change a few things up and try submitting again.
